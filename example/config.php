bcd1aa0540f6870a9e404c9afdbba2a36b683579
omfgtora <spaminol@gmail.com>
2023-05-15 00:46:49 -0700

    update example config

--- example/config.php
+++ example/config.php
@@ -1,12 +1,12 @@
 <?php
 return [
   'jwt_secret' => 'hunter2',
-  'validate_function' => function($login_hash, $instance) {
+  'validate_function' => function($login_hash, $instance, $secret) {
     $login = [ 
       'username' => 'admin',
       'password' => 'pass'
     ];
-    $valid_credentials = $instance->generate_credentials_hash($login['username'], $login['password']);
+    $valid_credentials = $instance->generate_credentials_hash($login['username'], $login['password'], $secret);
     return $login_hash === $valid_credentials;
   }
 ];
